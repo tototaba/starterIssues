305813aaadef9fc13a292fd7094a6d72ba2dc528
ndurano <ndurano@univerus.com>
2024-04-16 23:17:04 +0000

    Merged PR 8238: UNT-4158 DashBoard My Meetings Tab UDP grid
    implements the UDP grid to the "My Meetings" tab on Dashboard

--- src/routes/dashboard/MyMeetingsGrid.jsx
+++ src/routes/dashboard/MyMeetingsGrid.jsx
@@ -15,6 +15,7 @@ import {
 } from '@fluentui/react-icons';
 import { Typography } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { AmbientTemplateGrid } from 'unity-ambient-x-react';
 
 
 const MyMeetingsGrid = (props) => {
@@ -22,13 +23,13 @@ const MyMeetingsGrid = (props) => {
     const history = useHistory();
     const { gridApi, onGridReady } = useAgGridApi();
     const [meetings1, setMeetings] = useState([]);
+    const userId = user.id;
 
     const [{ data: meetings }, refetchMeetings] = useAxiosGet(
         process.env.REACT_APP_MEETING_MINUTES_API_BASE,
         `cpsmeeting/${user.id}/${user.currentTenantId}`,
         {},
     );
-
 
     const gridOptions = {
         frameworkComponents: {
@@ -70,9 +71,51 @@ const MyMeetingsGrid = (props) => {
         </PrimaryActionButton>
     )
 
+    const defaultSearchObject = [{
+        searchField: 'user_id',
+        searchOperator: '=',
+        searchValue: userId
+      }]
+
+      const renderConfigs = [
+        {
+          field: 'actions',
+          rendererName: 'hotListAddRenderer',
+          otherField: 'id',
+          callbackId: 'handleRowSelected',
+        },
+      ];
+    
+      const handleCellClick = (value, callback) => {
+        if (callback === 'handleRowSelected') {
+          history.push({
+            pathname: `/meetings/${value.groupId}/meeting/${value.meetingId}`,
+            state: { meeting: meetings }
+          })
+        }
+      };
+
     return (
         <>
-            <AmbientGridTemplate
+            <AmbientTemplateGrid
+                queryId={process.env.REACT_APP_MY_MEETINGS_DASHBOARD_QUERY_ID}
+                tenantId={user.currentTenantId}
+                userId={user.id}
+                gridId={'0b60eab9-9d55-4b7c-aa23-8484414e3bc5'}
+                productId={53}
+                accessToken={user.accessToken}
+                user={user}
+                callbackId="2"
+                cellClickHandler={handleCellClick.bind(this)}
+                cellClickHandlerX={handleCellClick.bind(this)}
+                renderConfigs={renderConfigs}
+                defaultSearchObject={defaultSearchObject}
+                viewActionClickHandler={handleCellClick.bind(this)}
+                fitGrid='fit'
+                showHotlistButton={false}
+            ></AmbientTemplateGrid>
+
+            {/* <AmbientGridTemplate
                 title='My Meetings'
                 primaryActionButton={addMeetingButton}
                 gridOptions={gridOptions}
@@ -84,7 +127,7 @@ const MyMeetingsGrid = (props) => {
                 height="calc(100vh - 200px)"
                 hideColumnTab
                 frameworkComponents={{ actionsRenderer: ActionsRenderer }}
-            />
+            /> */}
         </>
 
     );
